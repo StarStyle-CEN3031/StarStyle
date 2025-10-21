0706571c24506ab984b0754cad853409116be9bf
ntrejo12 <naydelintrejo12@gmail.com>
2025-10-21 16:24:03 -0400

    add manual cors set up

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,33 +12,33 @@ const app = express();
 
 //middleware
 // source: https://www.stackhawk.com/blog/fixing-no-access-control-allow-origin-header-present/
-// const allowedOrigins = ["http://localhost:3000", "https://star-style-git-naydelin-teafanys-projects.vercel.app"];
-// app.use((req, res, next) => {
-//     const origin = req.headers.origin;
+const allowedOrigins = ["http://localhost:3000", "https://star-style-git-naydelin-teafanys-projects.vercel.app"];
+app.use((req, res, next) => {
+    const origin = req.headers.origin;
     
-//     if (allowedOrigins.includes(origin)) {
-//         res.header('Access-Control-Allow-Origin', origin);
-//     }
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
 
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
 
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-//     if (req.method == 'OPTIONS') {
-//         return res.status(200).end();
-//     }
+    if (req.method == 'OPTIONS') {
+        return res.status(200).end();
+    }
 
-//     next();
-// })
+    next();
+})
 
-app.use(cors({
-    origin: ["http://localhost:3000", "https://star-style-git-naydelin-teafanys-projects.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: ["http://localhost:3000", "https://star-style-git-naydelin-teafanys-projects.vercel.app"],
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: ["Content-Type", "Authorization"],
+//     credentials: true
+// }));
 
-app.options('*', cors());
+// app.options('*', cors());
 
 app.use(bodyParser.json());
 
