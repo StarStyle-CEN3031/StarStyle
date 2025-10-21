05612419760f03e2c7921db88d3c0669f01c11a1
ntrejo12 <naydelintrejo12@gmail.com>
2025-10-21 16:36:21 -0400

    Fix signup.js

--- frontend/src/SignUp.js
+++ frontend/src/SignUp.js
@@ -23,7 +23,7 @@ function SignUp() {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: token,
+          Authorization: `Bearer ${token}`,
         },
       });
       
