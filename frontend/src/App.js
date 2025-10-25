892e8fa0de4bc5a73f7ac76cc5de477751617955
teafany <Tiffany121705@gmail.com>
2025-10-25 18:45:14 -0400

    feat: display logged-in user dynamically and add Profile page with logout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,64 @@
-import logo from "./logo.svg";
+import { useState, useEffect } from "react";
+import { auth } from "./config/firebase";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Login from "./Login";
 import SignUp from "./SignUp";
 import Quiz from "./Quiz";
 import Browse from "./Browse";
+import Profile from "./Profile";
+import { onAuthStateChanged } from "firebase/auth";
 
 function HomePage() {
+  const [user, setUser] = useState(null);
+
+  useEffect(function () {
+    function handleUser(firebaseUser) {
+      if (firebaseUser) {
+        setUser(firebaseUser);
+      } else {
+        setUser(null);
+      }
+    }
+
+    var listener = onAuthStateChanged(auth, handleUser);
+    return function () {
+      listener();
+    };
+  }, []);
+
+  function getUsername() {
+    if (!user) {
+      return "";
+    }
+    return user.email.split("@")[0];
+  }
+
+  let topbarContent;
+  if (user) {
+    topbarContent = (
+      <Link to="/profile" className="profile-link">
+        Hello, {getUsername()}!
+      </Link>
+    );
+  } else {
+    topbarContent = (
+      <div className="login-signup-buttons">
+        <Link to="/login">
+          <button>Login</button>
+        </Link>
+        <Link to="/signup">
+          <button>Sign Up</button>
+        </Link>
+      </div>
+    );
+  }
+
   return (
     <div className="homepage">
       <header className="topbar">
         <h1>StarStyle</h1>
-        <div className="login-signup-buttons">
-          <Link to="/login">
-            <button className="login">Login</button>
-          </Link>
-          <Link to="/signup">
-            <button className="signup">Sign Up</button>
-          </Link>
-        </div>
+        <div className="topbar-right">{topbarContent}</div>
       </header>
       <section className="welcome-banner">
         <h2>Celebrity Style Made Affordable</h2>
@@ -62,6 +102,7 @@ function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/browse" element={<Browse />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
@@ -71,4 +112,6 @@ export default App;
 
 /* references:
 www.w3schools.com/react/react_router.asp
+dev.to/dchowitz/react-firebase-a-simple-context-based-authentication-provider-1ool
+josecarrillo.me/creating-an-authentication-context-with-usecontext-in-react/
 */
